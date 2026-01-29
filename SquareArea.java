18ec7880f45a03147e7477b7a449b828358234d3
RajivRanganath <rajivranganath3@gmail.com>
2026-01-29 19:31:09 +0530

    Update SquareArea.java

--- SquareArea.java
+++ SquareArea.java
@@ -2,6 +2,9 @@ class SquareArea {
     public static void main(String[] args) {
         int side = 5;
         int area = side * side;
+        int side1=7;
+        int area1=side1*side1;
         System.out.println("Area of square = " + area);
+        System.out.println("Area of square = " + area1);
     }
 }
